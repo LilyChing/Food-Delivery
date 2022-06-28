026c52a8d228e665517a34b5144797adfaf43a3c
Ching Yan Lee <106129893+LilyChing@users.noreply.github.com>
2022-06-28 10:53:34 +0800

    add login function

---
+++ source/login.js
@@ -0,0 +1,23 @@
+var attempt = 3; // Variable to count number of attempts.
+// Below function Executes on click of login button.
+function validate() {
+    var checkUName = document.getElementById('checkuname').value;
+    var checkPW = document.getElementById('checkpw').value;
+    if (checkUName == "lily" && checkPW == "lily") {
+        alert("Login successfully");
+        return true;
+        window.location = "index.html"; // Redirecting to other page.
+    } else {
+        attempt--; // Decrementing by one.
+        document.getElementById("warning").innerHTML = "Wrong Username or Password. You have left "+ attempt + " attempt.";
+        // Disabling fields after 3 attempts.
+        if (attempt == 0) {
+            document.getElementById("checkuname").disabled = true;
+            document.getElementById("checkpw").disabled = true;
+            document.getElementById("loginbutton").disabled = true;
+            document.getElementById("warning").innerHTML = "Your account is blocked now. Please refresh the website."
+            return false;
+        }
+        return false;
+    }
+}
